f4122efe6142d8487d1de98cfa946451d39c7427
MrPus <pierrellensa@free.fr>
2014-05-03 00:17:28 +0200

    gitignore, menu
    ajout des png et jpg au gitignore
    ajout du menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+
+/* Pour ajouter un skin d'interface
+ * private GUISkin skin;
+	
+	void Start()
+	{
+		// Load a skin for the buttons
+		skin = Resources.Load("GUISkin") as GUISkin;
+	}*/
+
+	void OnGUI()
+	{
+		const int buttonwidth = 84;
+		const int buttonheight = 60;
+		
+		// Draw a button to start the game
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonwidth / 2),
+			(2 * Screen.height / 3) - (buttonheight / 2),
+			buttonwidth,
+			buttonheight
+			),
+			"Start!"
+			)
+			)
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel(1);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int type; // type du personnage ?
+
+	public int health;
+	public int movementspeed;
+	public int range;
+	public int damage;
+	public int attackspeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDestroy()
+	{
+		// Game Over.
+		transform.parent.gameObject.AddComponent<GameOverScript>();
+	}
+}
